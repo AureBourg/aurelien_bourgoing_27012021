7b41a284f9b404a6630bb99020359943772f8ca8
Aurelien <aurebourg@gmail.com>
2021-01-29 11:28:26 +0100

    preparing for updates

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 //Changer l'adresse de mangoose.connect par celle de mon cluster sur MongoDB et mettre le MDP à la place de <PASSWORD>
-mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://aurebourg:W4gkLm8OxgXXTEEi@cluster0.r1rom.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -19,5 +19,10 @@ app.use((req, res, next) => {
   });
 
 app.use(bodyParser.json());
+
+//Requête test
+app.use((req, res) => {
+  res.json({ message: 'Votre requête est reçue !' }); 
+});
 
 module.exports = app;
